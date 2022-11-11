a7d5a9c2517a6b7f5399e85374001d0c7fb5d625
robonexx <robertwagar@gmail.com>
2022-11-11 12:07:24 +0100

    I might have messed up something in my cleaning, I now copied everything from my "copy project from my private github where i also have the project"

--- js/index.js
+++ js/index.js
@@ -1,6 +1,6 @@
 // just using js to get the path lenght of the svg paths for animation with keyframes
-/* 
+
 var logo = document.querySelectorAll("#logo path");
 for (let i = 0; i < logo.length; i++) {
   console.log(`Letter ${i} is ${logo[i].getTotalLength()}`);
-} */
+}
